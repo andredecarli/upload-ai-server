1b8f82936a7b5bcf1ec869fc2d41ca1d0136988b
André De Carli <a.decarli89@gmail.com>
2023-09-14 17:12:44 -0300

    Setup Server

---
+++ src/routes/upload-video.ts
@@ -0,0 +1,53 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+import fastifyMultipart from '@fastify/multipart'
+import path from 'node:path'
+import { randomUUID } from 'node:crypto'
+import { promisify } from 'node:util'
+import { pipeline } from 'node:stream'
+import fs from 'node:fs'
+const pump = promisify(pipeline)
+
+export async function uploadVideosRoute(app: FastifyInstance) {
+  app.register(fastifyMultipart, {
+    limits: {
+      fileSize: 1024 * 1024 * 25, // 25Mb
+    },
+  })
+
+  app.post('/videos', async (request, reply) => {
+    const data = await request.file()
+
+    if (!data) {
+      return reply.status(400).send({ error: 'Missing file input.' })
+    }
+
+    const extension = path.extname(data.filename)
+    if (extension !== '.mp3') {
+      return reply
+        .status(400)
+        .send({ error: 'Invalid input type, please upload a MP3' })
+    }
+
+    const fileBaseName = path.basename(data.filename, extension)
+
+    const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`
+
+    const uploadDestination = path.resolve(
+      __dirname,
+      '../../tmp',
+      fileUploadName,
+    )
+
+    await pump(data.file, fs.createWriteStream(uploadDestination))
+
+    const video = await prisma.video.create({
+      data: {
+        name: data.filename,
+        path: uploadDestination,
+      },
+    })
+
+    return video
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,25 @@
-import {fastify} from "fastify"
+import { fastify } from 'fastify'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { uploadVideosRoute } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateAICompletionRoute } from './routes/generate-ai-completion'
+import fastifyCors from '@fastify/cors'
 
 const app = fastify()
 
-app.get('/', () => {
-  return "Hello World"
+app.register(fastifyCors, {
+  origin: '*',
 })
 
-app.listen({
-  port: 3333,
-}).then(() => { 
-  console.log('HTTP Server Running')
-})
+app.register(getAllPromptsRoute)
+app.register(uploadVideosRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running')
+  })
